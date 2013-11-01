42d337704ce98b9217206898f54c55e0b937a3bd
Masato Ikeda <masato.ikeda@gmail.com>
2013-11-02 08:53:12 +0900

    Reduce warning

--- ReFlanker/NSData+MD5Digest.m
+++ ReFlanker/NSData+MD5Digest.m
@@ -14,7 +14,7 @@
 - (NSString *)MD5Digest
 {
     unsigned char md5Buffer[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(self.bytes, self.length, md5Buffer);
+    CC_MD5(self.bytes, (unsigned int)self.length, md5Buffer);
     
     NSMutableString *output = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
